f3be7375578fe7298e64949c0122d919a91c6e85
Lars Brinkhoff <lars@nocrew.org>
2018-11-26 11:21:41 +0100

    Patch.

--- build.sh
+++ build.sh
@@ -4,8 +4,10 @@ rm -f rp06.disk
 bzcat 4.1_BSD*.tap.bz2 > 4.1_BSD_19810710-modified.tap
 
 test -x simh/BIN/vax780 || (cd simh; make vax780)
-
 PATH=$PWD/simh/BIN:$PATH
 export PATH
 
 expect -f tape.tcl
+
+gunzip chaos-4.1.tar.gz
+(cd simtools/converters/tar2mt; make tar2mtsimh)
