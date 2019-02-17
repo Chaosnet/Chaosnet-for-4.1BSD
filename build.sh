5a2eb12d4bc0f2e6c492dae7665a2a4358e9308a
Lars Brinkhoff <lars@nocrew.org>
2019-02-17 21:12:07 +0100

    Fix script.

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ test -x simh/BIN/vax780 || (cd simh; make vax780)
 PATH=$PWD/simh/BIN:$PATH
 export PATH
 
-expect -f tape.tcl
+expect -f install.tcl
 
-gunzip chaos-4.1.tar.gz
-(cd simtools/converters/tar2mt; make tar2mtsimh)
+gunzip -k chaos-4.1.tar.gz
+(cd simtools/converters/tar2mt; make tar2mt)
